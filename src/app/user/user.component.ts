b926c17206d9d25c91edadb34b3dba6ad3c7a92e
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2024-06-07 16:10:01 -0500

    output dynamic user data using string interpolation and property binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+// returns a random integer from 0 to (DUMMY_USERS.length - 1)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }

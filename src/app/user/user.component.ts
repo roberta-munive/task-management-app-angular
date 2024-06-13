2e6d6d29cfe41e214b52f037fbf09753afcc92a2
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2024-06-13 11:13:22 -0500

    Create tasks component and as a first step display the name of the selected user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  Input,
-  computed,
-  input,
-  Output,
-  EventEmitter,
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
